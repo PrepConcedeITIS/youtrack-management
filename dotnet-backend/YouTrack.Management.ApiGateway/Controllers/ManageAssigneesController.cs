7d6daa35bded331bb491171b560c354c44a1960e
PrepConcede <tagmirs_letter@mail.ru>
2022-04-10 19:05:00 +0300

    actualize project assignees gateway endpoint

--- dotnet-backend/YouTrack.Management.ApiGateway/Controllers/ManageAssigneesController.cs
+++ dotnet-backend/YouTrack.Management.ApiGateway/Controllers/ManageAssigneesController.cs
@@ -1,14 +1,29 @@
 ﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using YouTrack.Management.AssigneeActualize.Client;
 
 namespace YouTrack.Management.ApiGateway.Controllers
 {
     [Route("[controller]")]
-    public class ManageAssigneesController: ControllerBase
+    public class ManageAssigneesController : ControllerBase
     {
+        private readonly AssigneeActualizeClient _assigneeActualizeClient;
+
+        public ManageAssigneesController(AssigneeActualizeClient assigneeActualizeClient)
+        {
+            _assigneeActualizeClient = assigneeActualizeClient;
+        }
+
         [HttpPost("assign/{sprint}")]
         public async Task<IActionResult> Assign(string sprint)
         {
+            return Ok();
+        }
+
+        [HttpPost("actualizeProjectAssignees/{projectName}")]
+        public async Task<IActionResult> ActualizeProjectAssignees(string projectName)
+        {
+            await _assigneeActualizeClient.ActualizeAssigneesInDatabase();
             return Ok();
         }
     }
