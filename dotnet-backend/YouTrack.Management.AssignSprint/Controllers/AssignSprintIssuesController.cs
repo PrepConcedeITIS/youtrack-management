1ee21a04c0a14bbeb562733c026a226ebf77d6e6
PrepConcede <tagmirs_letter@mail.ru>
2022-04-13 01:09:52 +0300

    assigning issues

--- dotnet-backend/YouTrack.Management.AssignSprint/Controllers/AssignSprintIssuesController.cs
+++ dotnet-backend/YouTrack.Management.AssignSprint/Controllers/AssignSprintIssuesController.cs
@@ -21,8 +21,10 @@ namespace YouTrack.Management.AssignSprint.Controllers
         private readonly AssigneeActualizeClient _assigneeActualizeClient;
         private readonly MachineLearningClient _machineLearningClient;
         private readonly IIssueDistributionAlgorithm _issueDistributionAlgorithm;
+
         public AssignSprintIssuesController(AssigneeActualizeClient assigneeActualizeClient,
-            YouTrackClient youTrackClient, MachineLearningClient machineLearningClient, IIssueDistributionAlgorithm issueDistributionAlgorithm)
+            YouTrackClient youTrackClient, MachineLearningClient machineLearningClient,
+            IIssueDistributionAlgorithm issueDistributionAlgorithm)
         {
             _assigneeActualizeClient = assigneeActualizeClient;
             _youTrackClient = youTrackClient;
@@ -49,113 +51,12 @@ namespace YouTrack.Management.AssignSprint.Controllers
                 .GetPredictions(new PredictRequest(predictionRequestItems));
 
             var distributionResult = _issueDistributionAlgorithm.Handle(assignees, sprintIssues, predictionResult);
-            
-            var resultDictionary = new Dictionary<string, string>();
-            var marryingIterations = sprintIssues.Count / assignees.Count;
-            for (int i = 0; i < marryingIterations; i++)
-            {
-                var issuesIds = sprintIssues
-                    .Skip(i * assignees.Count)
-                    .Take(assignees.Count)
-                    .Select(x => x.IdReadable)
-                    .ToHashSet();
 
-                var assigneeMarriage = Marry(issuesIds, assignees, predictionResult);
+            var tasks = distributionResult
+                .Select(result => _youTrackClient.AssignToIssue(result.Key, result.Value));
+            await Task.WhenAll(tasks);
 
-                foreach (var (assignee, (_, issue)) in assigneeMarriage)
-                {
-                    resultDictionary.Add(issue, assignee);
-                }
-            }
-
-            var remainingIssuesIds = sprintIssues
-                .Skip(assignees.Count * marryingIterations)
-                .Select(x => x.IdReadable)
-                .ToHashSet();
-            if (remainingIssuesIds.Any())
-            {
-                var groupedByAssignee = predictionResult.Predictions
-                    .GroupBy(predict => predict.AssigneeLogin)
-                    .Select((group, index) =>
-                    {
-                        var assignee = group.Key;
-                        var issuesByResult = group
-                            .Where(x => remainingIssuesIds.Contains(x.Id))
-                            .OrderByDescending(x => x.Grade)
-                            .Select(x => x.Id)
-                            .ToList();
-                        return ((assignee, index), issuesByResult);
-                    })
-                    .ToDictionary(x => x.Item1.assignee);
-                var groupedByIssues = predictionResult.Predictions
-                    .Where(x => remainingIssuesIds.Contains(x.Id))
-                    .GroupBy(x => x.Id)
-                    .Select((group, index) =>
-                    {
-                        var assigneesByPreference =
-                            group.OrderByDescending(x => x.Grade).Select(x => x.AssigneeLogin).ToList();
-                        var issueId = group.Key;
-                        return ((issueId, index), assigneesByPreference);
-                    })
-                    .ToDictionary(x => x.Item1.issueId);
-                Dictionary<string, (bool married, string assignee)> issueMarriage =
-                    remainingIssuesIds.ToDictionary(x => x, _ => (false, default(string)));
-
-                while (issueMarriage.Any(x => !x.Value.married))
-                {
-                    var currentIssue = issueMarriage.First(x => !x.Value.married);
-                    var currentIssueId = currentIssue.Key;
-                    var preferredAssigneeByIssue = groupedByIssues[currentIssueId].assigneesByPreference[0];
-
-                    if (issueMarriage.All(x => x.Value.assignee != preferredAssigneeByIssue))
-                    {
-                        issueMarriage[currentIssueId] = (true, preferredAssigneeByIssue);
-                    }
-                    else
-                    {
-                        var previouslySelectedIssue = issueMarriage
-                            .Single(x => x.Value.assignee == preferredAssigneeByIssue && x.Value.married).Key;
-                        var assigneePreferences = groupedByAssignee[preferredAssigneeByIssue].issuesByResult;
-                        var previousIndex = assigneePreferences.IndexOf(previouslySelectedIssue);
-                        var currentIndex = assigneePreferences.IndexOf(currentIssueId);
-                        if (currentIndex < previousIndex)
-                        {
-                            issueMarriage[currentIssueId] = (true, preferredAssigneeByIssue);
-
-                            groupedByIssues[previouslySelectedIssue].assigneesByPreference
-                                .Remove(preferredAssigneeByIssue);
-
-                            issueMarriage[previouslySelectedIssue] = (false, null);
-                        }
-                        else
-                        {
-                            groupedByIssues[currentIssueId].assigneesByPreference
-                                .Remove(preferredAssigneeByIssue);
-                        }
-                    }
-                }
-
-                foreach (var (issueId, (_, assignee)) in issueMarriage)
-                {
-                    resultDictionary.Add(issueId, assignee);
-                }
-
-                // var mostSuitableAssignees = assignees;
-                // var issuesBestChoices = new Dictionary<string, List<string>>();
-                // foreach (var issuesId in remainingIssuesIds)
-                // {
-                //     var issueBestAssignees = predictionResult.Predictions
-                //         .Where(x => x.Id == issuesId)
-                //         .GroupBy(x => x.Grade)
-                //         .OrderByDescending(x => x.Key)
-                //         .First()
-                //         .Select(x=>x.AssigneeLogin)
-                //         .ToList();
-                //     issuesBestChoices.Add(issuesId, issueBestAssignees);
-                // }
-            }
-
-            return Ok(resultDictionary);
+            return Ok();
         }
 
         private Dictionary<string, (bool married, string issue)> Marry(HashSet<string> issuesIds,
