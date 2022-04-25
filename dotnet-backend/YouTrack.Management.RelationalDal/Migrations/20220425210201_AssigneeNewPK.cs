5f4b0617c93687a9636982869fedb3e4137fa217
PrepConcede <tagmirs_letter@mail.ru>
2022-04-26 00:05:27 +0300

    assignee new PK

---
+++ dotnet-backend/YouTrack.Management.RelationalDal/Migrations/20220425210201_AssigneeNewPK.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace YouTrack.Management.RelationalDal.Migrations
+{
+    public partial class AssigneeNewPK : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Assignees",
+                table: "Assignees");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ProjectName",
+                table: "Assignees",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Assignees",
+                table: "Assignees",
+                columns: new[] { "Id", "ProjectName" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Assignees",
+                table: "Assignees");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ProjectName",
+                table: "Assignees",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Assignees",
+                table: "Assignees",
+                column: "Id");
+        }
+    }
+}
--- dotnet-backend/YouTrack.Management.RelationalDal/YoutrackManagementDbContext.cs
+++ dotnet-backend/YouTrack.Management.RelationalDal/YoutrackManagementDbContext.cs
@@ -3,18 +3,20 @@ using YouTrack.Management.Shared.Entities.Issue;
 
 namespace YouTrack.Management.RelationalDal
 {
-    public class YoutrackManagementDbContext: DbContext
+    public class YoutrackManagementDbContext : DbContext
     {
-        public YoutrackManagementDbContext(DbContextOptions<YoutrackManagementDbContext> options): base(options)
+        public YoutrackManagementDbContext(DbContextOptions<YoutrackManagementDbContext> options) : base(options)
         {
         }
-        
+
         public DbSet<Assignee> Assignees { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Assignee>()
                 .Ignore(assignee => assignee.Competences);
+            modelBuilder.Entity<Assignee>()
+                .HasKey(x => new { x.Id, x.ProjectName });
         }
     }
 }
